e4d53605bf28d0dac69110478e23d3b4dc08e9a2
alokgoldy <alokkrgoldy@gmail.com>
2026-03-26 23:56:42 +0530

    feat: added binance websocket config

--- src/services/binance.ts
+++ src/services/binance.ts
@@ -1,5 +1,4 @@
 const BINANCE_WS = "wss://stream.binance.com:9443/ws";
 
-export const createTradeSocket = (symbol: string) => {
-    return new WebSocket(`${BINANCE_WS}/${symbol}@trade`);
-}
+export const createTradeSocket = (symbol: string): WebSocket =>
+  new WebSocket(`${BINANCE_WS}/${symbol}@trade`);
